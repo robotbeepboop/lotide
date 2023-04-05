3ce3a7db1e9152284aa0691d3a2002efd2e557d0
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-04-05 14:13:15 -0700

    Create assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,17 @@
+//const sum = function(x, y) {
+//  return x + y;
+//}
+
+//console.assert(sum(1, 8) === 3, "Womp womp");
+
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  console.assert(actual === expected, `${actual} !==  ${expected}`);
+  if (actual === expected) {
+    console.log(`Assertion passed: ${actual} === ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
