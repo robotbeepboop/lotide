e35c6bd7e847ec3b73341e08b93faf3f85a4cef4
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-04-05 14:37:50 -0700

    Create findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  console.assert(actual === expected, `${actual} !==  ${expected}`);
+  if (actual === expected) {
+    console.log(`Assertion passed: ${actual} === ${expected}`);
+  }
+};
+
+//takes in a value and an object
+const findKeyByValue = function(obj, val) {
+  for (var key of Object.keys(obj)) {
+    if(obj[key] === val) {
+      return key;
+    }
+  }
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
