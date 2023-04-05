a970c21a15c2e721063609b1985fa4e240245a3e
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-04-05 14:09:50 -0700

    Create assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,28 @@
+const eqArrays = function(arr1, arr2) {
+  //check if arrays are identical, empty, or of different lengths
+  if (arr1 === arr2) {
+    console.log(true);
+  } else if (arr1 == null || arr2 == null) {
+    console.log(false);
+  } else if (arr1.length !== arr2.length) {
+    console.log(false);
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        console.log(false);
+      } else {
+        console.log(true);
+      }
+    }
+  }
+  
+};
+
+const assertEqual = function(actual, expected) {
+  console.assert(actual === expected, `${actual} !==  ${expected}`);
+  if (actual === expected) {
+    console.log(`Assertion passed: ${actual} === ${expected}`);
+  }
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
