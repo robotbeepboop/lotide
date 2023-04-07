626e2f24c9480c6e1f42b66d3cb1afc52882b66b
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-04-06 17:54:20 -0700

    added the assert equal and then pushed

---
+++ findKey.js
@@ -0,0 +1,24 @@
+const findKey = (object, callback) => {
+  for (let key in object) { //check each key for the value
+    if (callback(object[key])) { //checks for truthy result
+      return key;
+    }
+  }
+}
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+//ASSERT EQUAL FUNCTION
+const assertEqual = function(actual, expected) {
+  console.assert(actual === expected, `${actual} !==  ${expected}`);
+  if (actual === expected) {
+    console.log(`Assertion passed: ${actual} === ${expected}`);
+  }
+};
