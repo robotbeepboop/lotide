dd997e238da3c6e86af076c7b323bb717fa69758
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-04-10 12:28:29 -0700

    added module export

--- assertObjectEquals.js
+++ assertObjectEquals.js
@@ -1,8 +1,12 @@
+const eqObjects = require('./eqObjects');
+
+// FUNCTION IMPLEMENTATION
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect; 
   //console.log(`Example label: ${inspect(actual)}`);
   if (eqObjects(actual, expected)) {
-    return (console.log(`✅✅✅ Assertion Passed: ${inspect(actual) } === ${inspect(expected) }`))
+    return (console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`))
   }
   return (console.log(`❌❌❌ Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`))
 };
+module.exports = assertObjectsEqual;
