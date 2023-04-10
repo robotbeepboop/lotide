36a4368cd20c48018ccf707b19bdf29ccf2102ac
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-04-10 09:47:02 -0700

    refactored code using modules

--- tail.js
+++ tail.js
@@ -1,10 +1,5 @@
+const assertEqual = require('../assertEqual');
+
 const tail = function(arr) {
   arrTail = arr.slice(1);
 }
-
-const assertEqual = function(actual, expected) {
-  console.assert(actual === expected, `${actual} !==  ${expected}`);
-  if (actual === expected) {
-    console.log(`Assertion passed: ${actual} === ${expected}`);
-  }
-};
