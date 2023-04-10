53e2e42d8ab24f708d208599f3638c0e8b6c0acb
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-04-10 12:37:55 -0700

    module export added

--- findKey.js
+++ findKey.js
@@ -1,3 +1,4 @@
+const assertEqual = require('./assertEqual');
 const findKey = (object, callback) => {
   for (let key in object) { //check each key for the value
     if (callback(object[key])) { //checks for truthy result
@@ -6,14 +7,13 @@ const findKey = (object, callback) => {
   }
 }
 
-findKey({
+/*findKey({
   "Blue Hill": { stars: 1 },
   "Akaleri":   { stars: 3 },
   "noma":      { stars: 2 },
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}, x => x.stars === 2) // => "noma"
+}, x => x.stars === 2)*/ // => "noma"
 
-//ASSERT EQUAL FUNCTION
-const assertEqual = require('./assertEqual');
+module.exports = findKey;
